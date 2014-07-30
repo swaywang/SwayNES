6ea9c32955a26c434c55af3b061b7fe951c63cae
Sway <sway.khwang@gmail.com>
2014-07-30 11:45:37 +0800

    Move CPU init to setUp function

--- src/unittest/cpu_test_case.py
+++ src/unittest/cpu_test_case.py
@@ -9,19 +9,18 @@ from cpu.cpu import CPU
 
 class CPUTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        self.cpu = CPU()
 
     def tearDown(self):
         pass
 
     def test_cpu_registers_init(self):
-        cpu = CPU()
-        self.assertEquals(0xc000, cpu.registers["pc"].read())
-        self.assertEquals(0x00, cpu.registers["a"].read())
-        self.assertEquals(0x00, cpu.registers["x"].read())
-        self.assertEquals(0x00, cpu.registers["y"].read())
-        self.assertEquals(0xfd, cpu.registers["sp"].read())
-        self.assertEquals(0x24, cpu.registers["p"].read())
+        self.assertEquals(0xc000, self.cpu.registers["pc"].read())
+        self.assertEquals(0x00, self.cpu.registers["a"].read())
+        self.assertEquals(0x00, self.cpu.registers["x"].read())
+        self.assertEquals(0x00, self.cpu.registers["y"].read())
+        self.assertEquals(0xfd, self.cpu.registers["sp"].read())
+        self.assertEquals(0x24, self.cpu.registers["p"].read())
 
 if __name__ == '__main__':
     unittest.main()
